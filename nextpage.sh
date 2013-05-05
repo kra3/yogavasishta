3d11e19e0bd1e03dde8157a967f4e3c542bb9adc
navaneethkn <navaneethkn@gmail.com>
2013-05-05 11:11:41 +0530

    Fixing editor opening in multiple OS

--- nextpage.sh
+++ nextpage.sh
@@ -4,5 +4,10 @@ echo "\section{ദിവസം $1}
 
 }
 " > "day$1".tex
-gedit "day$1".tex
+unamestr=`uname`
+if [ "$unamestr" == "Darwin" ]; then
+    open -a TextEdit "day$1".tex
+else
+    gedit "day$1".tex
+fi
 
